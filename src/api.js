70051cbce6b061691989bfbbe0a8d78ffbae7abb
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-09-27 20:57:53 +0900

    API Verbs part One

--- src/api.js
+++ src/api.js
@@ -9,3 +9,15 @@ const api = axios.create({
 });
 
 api.get("tv/popular");
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+};
