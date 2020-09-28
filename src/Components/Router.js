aa54ba7f5ed4786de72491d607c2fefc14dd75e4
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-09-28 13:48:23 +0900

    Detail Container part One

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -7,8 +7,9 @@ import {
 } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
-import Search from "Routes/Search";
 import Header from "Components/Header";
+import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
@@ -16,8 +17,10 @@ export default () => (
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
-        <Route path="/search" exact component={Search} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/tv/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -7,6 +7,20 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+  }
+
   render() {
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
