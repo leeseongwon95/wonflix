782dc19f9f0e39dcf74db641e83b4165b7c7a2f8
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-01 16:14:13 +0900

    Updates

---
+++ precache-manifest.3e2478357bf806bf65b752394508929b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fb42a889f41445bdeae99ecf380d34",
+    "url": "/wonflix/index.html"
+  },
+  {
+    "revision": "18a26984c97cf515f050",
+    "url": "/wonflix/static/js/2.baf349df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wonflix/static/js/2.baf349df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d66c1a3138b87ca71e",
+    "url": "/wonflix/static/js/main.65463067.chunk.js"
+  },
+  {
+    "revision": "a7aa8b39bae7f6727dc2",
+    "url": "/wonflix/static/js/runtime-main.c629b1d4.js"
+  }
+]);
