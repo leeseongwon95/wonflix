5223876b72e33c3caa04776cb2d8203562adc89e
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-01 12:22:11 +0900

    Detail Container part One

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -5,8 +5,9 @@ import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
 import Poster from "../../Components/Poster";
+
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 20px;
 `;
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
   loading ? (
